d13bc1b6a1265bc02b44e8f85c90b3bcd56a5914
scramboss <scramby1337@hotmail.com>
2025-01-08 09:41:18 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -4,13 +4,15 @@
 #
 # The full license is in the file LICENSE.txt, distributed with this software.
 #-----------------------------------------------------------------------------
-''' Define the Bokeh application class.
+''' Provide Bokeh model "building block" classes.
 
-A *Bokeh Application* is a lightweight factory for creating Bokeh Documents.
-For example, whenever a new client connects to a Bokeh Server, the server uses
-the ``Application`` to generate a unique new document, to service  the client
-session. The application performs this task by invoking the ``modify_document``
-method of any ``Handler`` objects that it is configured with.
+One of the central design principals of Bokeh is that, regardless of
+how the plot creation code is spelled in Python (or other languages),
+the result is an object graph that encompasses all the visual and
+data aspects of the scene. Furthermore, this *scene graph* is to be
+serialized, and it is this serialized graph that the client library
+BokehJS uses to render the plot. The low-level objects that comprise
+a Bokeh scene graph are called :ref:`Models <bokeh.model>`.
 
 '''
 
@@ -27,14 +29,118 @@ log = logging.getLogger(__name__)
 #-----------------------------------------------------------------------------
 
 # Bokeh imports
-from .application import Application
+from ..model import Model
+from . import (
+    annotations,
+    axes,
+    callbacks,
+    canvas,
+    comparisons,
+    coordinates,
+    css,
+    expressions,
+    filters,
+    formatters,
+    glyphs,
+    graphs,
+    grids,
+    labeling,
+    layouts,
+    map_plots,
+    mappers,
+    misc,
+    nodes,
+    plots,
+    ranges,
+    renderers,
+    scales,
+    selections,
+    selectors,
+    sources,
+    text,
+    textures,
+    tickers,
+    tiles,
+    tools,
+    transforms,
+    ui,
+    widgets,
+)
+from .annotations import *
+from .axes import *
+from .callbacks import *
+from .canvas import *
+from .comparisons import *
+from .coordinates import *
+from .css import *
+from .expressions import *
+from .filters import *
+from .formatters import *
+from .glyphs import *
+from .graphs import *
+from .grids import *
+from .labeling import *
+from .layouts import *
+from .map_plots import *
+from .mappers import *
+from .misc import *
+from .nodes import *
+from .plots import *
+from .ranges import *
+from .renderers import *
+from .scales import *
+from .selections import *
+from .selectors import *
+from .sources import *
+from .text import *
+from .textures import *
+from .tickers import *
+from .tiles import *
+from .tools import *
+from .transforms import *
+from .ui import *
+from .widgets import *
 
 #-----------------------------------------------------------------------------
 # Globals and constants
 #-----------------------------------------------------------------------------
 
 __all__ = (
-    'Application',
+    "Model",
+    *annotations.__all__,
+    *axes.__all__,
+    *callbacks.__all__,
+    *canvas.__all__,
+    *comparisons.__all__,
+    *coordinates.__all__,
+    *css.__all__,
+    *expressions.__all__,
+    *filters.__all__,
+    *formatters.__all__,
+    *glyphs.__all__,
+    *graphs.__all__,
+    *grids.__all__,
+    *labeling.__all__,
+    *layouts.__all__,
+    *map_plots.__all__,
+    *mappers.__all__,
+    *misc.__all__,
+    *nodes.__all__,
+    *plots.__all__,
+    *ranges.__all__,
+    *renderers.__all__,
+    *scales.__all__,
+    *selections.__all__,
+    *selectors.__all__,
+    *sources.__all__,
+    *text.__all__,
+    *textures.__all__,
+    *tickers.__all__,
+    *tiles.__all__,
+    *tools.__all__,
+    *transforms.__all__,
+    *ui.__all__,
+    *widgets.__all__,
 )
 
 #-----------------------------------------------------------------------------
